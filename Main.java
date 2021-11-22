eb4859acfe5f1c139f1128b17165cc80470633d0
Gabriel wallace <62078328+gabrielwallaceBDS@users.noreply.github.com>
2021-11-22 19:12:47 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -156,75 +156,7 @@ public class EncriptaDecriptaRSA {
       e.printStackTrace();
     }
   }
-}import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import javax.crypto.Cipher;
-
-
-public class EncriptaDecriptaRSA {
-
-  public static final String ALGORITHM = "RSA";
-
-  /**
-   * Local da chave privada no sistema de arquivos.
-   */
-  public static final String PATH_CHAVE_PRIVADA = "C:/keys/private.key";
-
-  /**
-   * Local da chave pública no sistema de arquivos.
-   */
-  public static final String PATH_CHAVE_PUBLICA = "C:/keys/public.key";
-
-  /**
-   * Gera a chave que contém um par de chave Privada e Pública usando 1025 bytes.
-   * Armazena o conjunto de chaves nos arquivos private.key e public.key
-   */
-  public static void geraChave() {
-    try {
-      final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
-      keyGen.initialize(1024);
-      final KeyPair key = keyGen.generateKeyPair();
-
-      File chavePrivadaFile = new File(PATH_CHAVE_PRIVADA);
-      File chavePublicaFile = new File(PATH_CHAVE_PUBLICA);
-
-      // Cria os arquivos para armazenar a chave Privada e a chave Publica
-      if (chavePrivadaFile.getParentFile() != null) {
-        chavePrivadaFile.getParentFile().mkdirs();
-      }
-
-      chavePrivadaFile.createNewFile();
-
-      if (chavePublicaFile.getParentFile() != null) {
-        chavePublicaFile.getParentFile().mkdirs();
-      }
-
-      chavePublicaFile.createNewFile();
-
-      // Salva a Chave Pública no arquivo
-      ObjectOutputStream chavePublicaOS = new ObjectOutputStream(
-          new FileOutputStream(chavePublicaFile));
-      chavePublicaOS.writeObject(key.getPublic());
-      chavePublicaOS.close();
-
-      // Salva a Chave Privada no arquivo
-      ObjectOutputStream chavePrivadaOS = new ObjectOutputStream(
-          new FileOutputStream(chavePrivadaFile));
-      chavePrivadaOS.writeObject(key.getPrivate());
-      chavePrivadaOS.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-  }
-
+}
   /**
    * Verifica se o par de chaves Pública e Privada já foram geradas.
    */
